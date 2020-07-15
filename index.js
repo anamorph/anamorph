1479bc16edb22f7538807a094e9ef8c575549680
Nicolas David <davinico@amazon.com.bh>
2020-07-15 23:07:25 +0300

    fix: workflow -> build status icon !

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const md = require("markdown-it")({
   const fs = require("fs");
    
   (async () => {
-    blurb = `Hello World !\n\nI am Nicolas (Nico) David, a ${currentRole} at [${currentCompany}](${currentCompanyUrl}) , based in ${currentCountry}.\n\n![](https://github.com/anamorph/anamorph/workflows/build/badge.svg)`;
+    blurb = `Hello World !\n\nI am Nicolas (Nico) David, a ${currentRole} at [${currentCompany}](${currentCompanyUrl}) , based in ${currentCountry}.\n\n![](https://github.com/anamorph/anamorph/workflows/README.md%20builder/badge.svg)`;
   
     const result = md.render(blurb);
   
