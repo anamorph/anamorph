ed41ec0c7efd2eafa6ce48f017641a7f169036b0
Nicolas David <davinico@amazon.com.bh>
2020-07-15 22:56:37 +0300

    release: v1.0

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const md = require("markdown-it")({
-    html: true, // Enable HTML tags in source
-    breaks: true, // Convert '\n' in paragraphs into <br/>
+    html: true,
+    breaks: true,
+    linkify: true,
   });
+  
+  // details
+  const currentRole = "Digital Innovation Senior consultant";
+  const currentCompany = "Amazon Web Services";
+  const currentCompanyUrl = "https://aws.amazon.com/aws-me/";
+  const currentCountry = "Bahrain";
+
   const fs = require("fs");
    
   (async () => {
-    text = `Hello World !\n\nI am Nicolas (Nico) David, a Digital Innovation Senior consultant \n\n![](https://github.com/anamorph/anamorph/workflows/build/badge.svg)`;
+    blurb = `Hello World !\n\nI am Nicolas (Nico) David, a ${currentRole} at [${currentCompany}](${currentCompanyUrl}) , based in ${currentCountry}.\n\n![](https://github.com/anamorph/anamorph/workflows/build/badge.svg)`;
   
-    const result = md.render(text);
+    const result = md.render(blurb);
   
     fs.writeFile("README.md", result, function (err) {
       if (err) return console.log(err);
